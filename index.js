af4090c15364fc983af14f6e363602b406b56477
Angel <55033374+Kashotex@users.noreply.github.com>
2020-01-09 20:59:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const config = require("./config.json");
 client.config = config;
 
 client.on('ready', () => {
-  console.log(times+`\x1b[33m%s\x1b[0m`,'[WARN]','\x1b[0m','Connexion en cours...');
-  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m', 'Connexion à l\'API Discord.js effectuée');
-  console.log(times+`\x1b[36m%s\x1b[0m`,'[INFO]', '\x1b[0m','Connecté sur ' + client.user.username + '#' + client.user.discriminator);
-  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Chargement terminé');
-  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Prêt et connecté - Développé par Emmanuel.');
+  console.log(times+`\x1b[33m%s\x1b[0m`,'[WARN]','\x1b[0m','Currently Connecting. . .');
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m', 'Connected to the Discord.js API successfully');
+  console.log(times+`\x1b[36m%s\x1b[0m`,'[INFO]', '\x1b[0m','Connected to ' + client.user.username + '#' + client.user.discriminator);
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Loading complete');
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Ready and connected - Developed by Emmanuel');
 
   antispam(client, {
     warnBuffer: 3,
     maxBuffer: 5,
     interval: 2000,
-    warningMessage: "le spam n'est pas autorisé. Si vous persistez, vous serais banni !",
-    banMessage: "a été banni pour spam.",
-    maxDuplicatesWarning: 7,
+    warningMessage: "Spam is not allowed. If you persist, you will be banned!",
+    banMessage: "has been banned for spamming.",
+    maxDuplicatesWarning: 5,
     maxDuplicatesBan: 10,
     deleteMessagesAfterBanForPastDays: 7,
-    exemptUsers: ["Blizzard#4453", "Emmanuel#1411"]
+    exemptUsers: ["Kashotex#1634"]
   });
   
 client.login(config.token);
